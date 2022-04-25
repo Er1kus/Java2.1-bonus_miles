55c729c38b98b167d3f51e02eb65c51b14a6ddbc
Er1kus <goryaev86@mail.ru>
2022-04-25 22:44:03 +0300

    bonus calculation

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketCost = 5600;
+        int mileCost = 20;
+        int mile = ticketCost / mileCost;
+        System.out.println("Количество начисленных миль: " + mile);
+
+    }
+}
